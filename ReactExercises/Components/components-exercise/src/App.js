34a416be2d995093cd025f3bb4aa3b47c3620cc3
Pavel Romanov <pavelromanov@Pavels-MacBook-Pro.local>
2023-04-01 14:52:01 +0300

    components demo

---
+++ ReactExercises/Components/components-exercise/src/App.js
@@ -0,0 +1,71 @@
+import "./App.css";
+import { Fragment, useEffect, useState } from "react";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Search from "./components/Search";
+import UserList from "./components/UserList";
+import * as userService from "./services/userService";
+
+function App() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    userService
+      .getAll()
+      .then((users) => {
+        setUsers(users);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  const onDelete = async (userId) => {
+    await userService.deleteUser(userId);
+    setUsers(users.filter((u) => u._id !== userId));
+  };
+
+  const onUserCreateSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData);
+
+    const newUser = await userService.createUser(data);
+
+    setUsers((state) => [...state, newUser]);
+  };
+
+  const onUserEditSubmit = async (e, userId) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const data = Object.fromEntries(formData);
+
+    const updatedUser = await userService.editUser(data, userId);
+
+    setUsers((state) =>
+      state.map((u) => (u._id === updatedUser._id ? updatedUser : u))
+    );
+  };
+
+  return (
+    <Fragment>
+      <Header></Header>
+      <main className="main">
+        <section className="card users-container">
+          <Search></Search>
+          <UserList
+            users={users}
+            onDelete={onDelete}
+            onUserCreateSubmit={onUserCreateSubmit}
+            onUserEditSubmit={onUserEditSubmit}
+          ></UserList>
+        </section>
+      </main>
+      <Footer></Footer>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ ReactExercises/Components/components-exercise/src/services/userService.js
@@ -0,0 +1,80 @@
+const baseUrl = "http://localhost:3005/api/users";
+
+export const getAll = async () => {
+  const response = await fetch(baseUrl);
+  const result = await response.json();
+
+  return result.users;
+};
+
+export const getOne = async (userId) => {
+  const response = await fetch(`${baseUrl}/${userId}`);
+  const result = await response.json();
+
+  return result.user;
+};
+
+export const deleteUser = async (userId) => {
+  await fetch(`${baseUrl}/${userId}`, { method: "Delete" })
+    .then((res) => {
+      console.log("Deleted user with id = " + userId);
+      return userId;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+export const createUser = async (data) => {
+  const user = {
+    firstName: data.firstName,
+    lastName: data.lastName,
+    email: data.email,
+    phoneNumber: data.phoneNumber,
+    imageUrl: data.imageUrl,
+    address: {
+      country: data.country,
+      city: data.city,
+      street: data.street,
+      streetNumber: data.streetNumber,
+    },
+  };
+
+  const requestOptions = {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(user),
+  };
+
+  const response = await fetch(`${baseUrl}`, requestOptions);
+  const result = await response.json();
+
+  return result.user;
+};
+
+export const editUser = async (data, userId) => {
+  const editedUser = {
+    firstName: data.firstName,
+    lastName: data.lastName,
+    email: data.email,
+    phoneNumber: data.phoneNumber,
+    imageUrl: data.imageUrl,
+    address: {
+      country: data.country,
+      city: data.city,
+      street: data.street,
+      streetNumber: data.streetNumber,
+    },
+  };
+
+  const requestOptions = {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(editedUser),
+  };
+
+  const response = await fetch(`${baseUrl}/${userId}`, requestOptions);
+  const result = await response.json();
+
+  return result.user;
+};
